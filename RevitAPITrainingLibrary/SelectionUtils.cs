5853171ee74b5d766dc529cc87f17cfaf5ab612e
andreyzr43 <andreyzr43@yandex.ru>
2023-08-04 07:53:56 +0300

    1

---
+++ RevitAPITrainingLibrary/SelectionUtils.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class SelectionUtils
+    {
+        public static Element PickObject(ExternalCommandData commandData,string massage= "Выберете элемент")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObject = uidoc.Selection.PickObject(ObjectType.Element, massage);
+            var oElement = doc.GetElement(selectedObject);
+            return oElement;
+        }
+    }
+}
