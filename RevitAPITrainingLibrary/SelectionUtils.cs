06497a1c0dccb37c92a19046c26b415b8561c145
andreyzr43 <andreyzr43@yandex.ru>
2023-08-07 17:58:07 +0300

    дз

--- RevitAPITrainingLibrary/SelectionUtils.cs
+++ RevitAPITrainingLibrary/SelectionUtils.cs
@@ -24,15 +24,15 @@ namespace RevitAPITrainingLibrary
             return oElement;
         }
 
-        public static List<Element> PickedObjects(ExternalCommandData commandData, string massage = "Выберете элемент")
+        public static List<Element> PickObjects(ExternalCommandData commandData, string message = "Выберите элементы")
         {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-            Reference selectedObjects = uidoc.Selection.PickObject(ObjectType.Element, massage);
-            List<Element> elementList = selectedObjects.Select(selectedObjects=>doc.GetElement(selectedObjects)).ToList();
-            return ;
+            var selectedObjects = uidoc.Selection.PickObjects(ObjectType.Element, message);
+            List<Element> elementList = selectedObjects.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
+            return elementList;
         }
 
     }
