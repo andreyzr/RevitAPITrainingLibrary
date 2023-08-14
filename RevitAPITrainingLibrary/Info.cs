b1c2487991572d69048102f7303f769c8d2d94e8
andreyzr43 <andreyzr43@yandex.ru>
2023-08-14 16:15:09 +0300

    Добалено ДЗ

--- RevitAPITrainingLibrary/Info.cs
+++ RevitAPITrainingLibrary/Info.cs
@@ -54,5 +54,6 @@ namespace RevitAPITrainingLibrary
 
             return doors;
         }
+
     }
 }
---
+++ RevitAPITrainingLibrary/WallTools.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class WallTools
+    {
+        public static List<WallType> GetWallTypes(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<WallType> wallTypes = new FilteredElementCollector(doc)
+                                                        .OfClass(typeof(WallType))
+                                                        .Cast<WallType>()
+                                                        .ToList();
+
+            return wallTypes;
+        }
+    }
+}
