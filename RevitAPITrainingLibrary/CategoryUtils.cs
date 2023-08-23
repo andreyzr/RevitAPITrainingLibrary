3f36bac2b9f7d5399724dc4c6fee49f739cbf210
andreyzr43 <andreyzr43@yandex.ru>
2023-08-23 07:55:18 +0300

    1

---
+++ RevitAPITrainingLibrary/CategoryUtils.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class CategoryUtils
+    {
+        public static List<Category> GetCategories(Document doc)
+        {
+            var categoryList = new List<Category>();
+            Categories categories = doc.Settings.Categories;
+            foreach (Category category in categories)
+            {
+                categoryList.Add(category);
+            }
+            return categoryList.OrderBy(c => c.Name).ToList();
+        }
+    }
+}
---
+++ RevitAPITrainingLibrary/ViewsUtils.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class ViewsUtils
+    {
+        public static List<ViewPlan> GetFloorPlanViews(Document doc)
+        {
+            var views
+               = new FilteredElementCollector(doc)
+                   .OfClass(typeof(ViewPlan))
+                   .Cast<ViewPlan>()
+                   .Where(p => p.ViewType == ViewType.FloorPlan)
+                   .ToList();
+            return views;
+        }
+    }
+}
